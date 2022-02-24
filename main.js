b9b846ea80ee3d5b283716a559a67b0a69d119d0
jvj13122010 <80154440+jvj13122010@users.noreply.github.com>
2022-02-24 17:41:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+objects = [];
+statuss = "";
+
+function preload(){
+    video = createVideo("video.mp4");
+}
+
+function setup() {
+    canvas = createCanvas(400,400);
+    canvas.center();
+    video.hide();
+}
+
+function start() {
+    
+}
